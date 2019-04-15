012fb11ecafea30718b70cf38291cc6e46a89539
bootler <aboolean123@hotmail.com>
2019-04-14 23:16:02 -0400

    Create initial mock

--- src/App.js
+++ src/App.js
@@ -2,12 +2,92 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function Square(props) {
+  return(
+    <button className="square">
+      {props.value}
+    </button>
+  );
+}
+
+class Board extends Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="status">Next Player: Black</div> 
+        <div className="board-row">
+            <Square value="R"/>
+            <Square />
+            <Square />
+            <Square />
+            <Square value="B"/>
+            <Square />
+            <Square />
+            <Square />
+          </div>
+          <div className="board-row">
+            <Square />
+            <Square />
+            <Square />
+            <Square />
+            <Square />
+            <Square />
+            <Square value="R"/>
+            <Square />
+          </div>
+          <div className="board-row">
+            <Square />
+            <Square />
+            <Square value="R"/>
+            <Square value="R"/>
+            <Square />
+            <Square />
+            <Square />
+            <Square />
+          </div>
+          <div className="board-row">
+            <Square />
+            <Square value="B"/>
+            <Square />
+            <Square />
+            <Square />
+            <Square />
+            <Square value="B"/>
+            <Square />
+          </div>
+          <div className="board-row">
+          <Square value="B"/>
+            <Square />
+            <Square />
+            <Square />
+            <Square />
+            <Square />
+            <Square />
+            <Square />
+          </div>
+          <div className="board-row">
+            <Square />
+            <Square />
+            <Square />
+            <Square />
+            <Square />
+            <Square />
+            <Square />
+            <Square />
+          </div>
+      </div>
+    );
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <h1>React Four</h1>
+          
+          {/*<img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload. Here is a test
           </p>
@@ -18,8 +98,15 @@ class App extends Component {
             rel="noopener noreferrer"
           >
             Learn React
-          </a>
+    </a> */}
         </header>
+        <main className="main-bg">
+            <Board />
+            <section>
+              <button>Save Game</button>
+              <button>Load Game</button>
+            </section>
+        </main>        
       </div>
     );
   }
